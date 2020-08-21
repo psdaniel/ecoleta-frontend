1489ce045dc5a827bae966e48b8db64aaf95a25a
Daniel <daniel.n10@hotmail.com>
2020-08-21 09:07:32 -0300

    integração do mapa no formulário e a busca dos itens de coleta na API

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api'
 
 import './styles.css'
 
 import logo from '../../assets/logo.svg'
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([])
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data)
+        })
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -18,10 +34,93 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
-                <h1>Cadastro do<br/> ponto de coleta</h1>
-            </form>
-        </div>
+    <form>
+        <h1>Cadastro do<br/> ponto de coleta</h1>
+
+        <fieldset>
+            <legend>
+                <h2>Dados</h2>
+            </legend>
+            
+            <div className="field">
+                <label htmlFor="name">Nome da entidade</label>
+                <input 
+                type="text"
+                name="name"
+                id="name"
+                />
+            </div>
+
+            <div className="field-group">
+                <div className="field">
+                    <label htmlFor="email">E-mail</label>
+                    <input 
+                    type="email"
+                    name="email"
+                    id="email"
+                    />
+            </div>
+                <div className="field">
+                    <label htmlFor="whatsapp">Whatsapp</label>
+                    <input 
+                    type="text"
+                    name="whatsapp"
+                    id="whatsapp"
+                    />
+                </div>
+            </div>
+        </fieldset>
+        
+        <fieldset>
+            <legend>
+                <h2>Endereço</h2>
+                <span>Selecione o endereço no mapa</span>
+            </legend>
+
+            <Map center={[-22.9758144,-43.339121]} zoom={15}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+                <Marker position={[-22.9758144,-43.339121]} />
+            </Map>
+
+            <div className="field-group">
+                <div className="field">
+                    <label htmlFor="uf">Estado(UF)</label>
+                    <select name="uf" id="uf">
+                        <option value="0">Selecione uma UF</option>
+                    </select>
+                </div>
+                <div className="field">
+                    <label htmlFor="city">Cidade</label>
+                    <select name="city" id="city">
+                        <option value="0">Selecione uma Cidade</option>
+                    </select>
+                </div>
+            </div>
+        </fieldset>
+        
+        <fieldset>
+            <legend>
+                <h2>Ítens de coleta</h2>
+                <span>Selecione um ou mais ítens abaixo</span>
+            </legend>
+
+            <ul className="items-grid">
+                {items.map(item => (                
+                <li key={item.id}>
+                    <img src={item.image_url} alt={item.title}/>
+                <span>{item.title}</span>
+                </li>
+                ))}
+            </ul>
+        </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
+    </form>
+</div>
     )
 }
 
