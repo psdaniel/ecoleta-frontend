4b7ad9f3c3330699e2bef99309cd749d5d546555
Daniel <daniel.n10@hotmail.com>
2020-08-21 10:10:47 -0300

    ..

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -30,6 +30,8 @@ const CreatePoint = () => {
 
 
     const [selectedUf, setSelectedUf] = useState('0')
+    const [selectedCity, setSelectedCity] = useState('0')
+
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -60,6 +62,12 @@ const CreatePoint = () => {
         const uf = event.target.value
     
         setSelectedUf(uf)
+    }
+
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = event.target.value
+    
+        setSelectedCity(city)
     }
 
     return (
@@ -141,7 +149,12 @@ const CreatePoint = () => {
                 </div>
                 <div className="field">
                     <label htmlFor="city">Cidade</label>
-                    <select name="city" id="city">
+                    <select 
+                        name="city" 
+                        id="city"
+                        value={selectedCity}
+                        onChange={handleSelectCity}
+                    >
                         <option value="0">Selecione uma Cidade</option>
                         {cities.map(city => (
                             <option key={city} value={city}>{city}</option>
