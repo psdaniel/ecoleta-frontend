bf7fe513650c2713c757389bfc9c0bd59bd548bf
Daniel <daniel.n10@hotmail.com>
2020-08-25 12:08:02 -0300

    finalizando o upload de imagens no backend

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -4,7 +4,11 @@ import { FiUpload} from 'react-icons/fi'
 
 import './styles.css'
 
-const Dropzone = () => {
+interface Props {
+    onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
     const [selectedFileUrl, setSelectedFileUrl] = useState('')
 
     const onDrop = useCallback(acceptedFiles => {
@@ -13,7 +17,8 @@ const Dropzone = () => {
         const fileUrl = URL.createObjectURL(file)
     
         setSelectedFileUrl(fileUrl)
-    }, [])
+        onFileUploaded(file)
+    }, [onFileUploaded])
 
     const {getRootProps, getInputProps } = useDropzone({
         onDrop,
